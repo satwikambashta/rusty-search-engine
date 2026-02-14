caca41b05fc6cd6253c210b7f2d8279eb4631537
satwikambashta <simmysatwik03@gmail.com>
2026-02-14 18:32:49 +0530

    set up buffer

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use std::env;
 use std::fs;
 use std::process::exit;
+use xml::reader::{XmlEvent,EventReader};
+
+fn parse_xml_file(file_path: &str) -> io::Result<String> {
+    
+}
 
 fn main() {
     let file_path = "docs.gl/gl4/glClear.xhtml";
@@ -10,10 +15,34 @@ fn main() {
     //     exit(1);
     // }
     // let file_path = &args[1];
-    let contents = fs::read_to_string(file_path).unwrap_or_else(|err| {
+    let file = fs::File::open(file_path).unwrap_or_else(|err| {
         eprintln!("Failed to read file: {err}");
+        exit(1);
+    });
+    let reader = EventReader::new(file);
+
+    let mut buffer = String::new();
+    for event in reader {
+        // match event {
+        //     Ok(event) => println!("Event: {event:?}"),
+        //     Err(err) => eprintln!("Error: {err}"),
+        // }
+        let event = event.unwrap_or_else(|err| {
+            eprintln!("Failed to read event: {err}");
+            exit(1);
+        });
+        if let XmlEvent::Characters(content) = event {
+            // println!("Content: {content}");
+            buffer.push_str(&content);
+        }
+        // println!("Event: {event:?}");
+    }
+
+    let contents = fs::read_to_string(file_path).unwrap_or_else(|err| {
+        // eprintln!("Failed to read file: {err}");
         String::new()
     });
-    println!("Length of file: {}", contents.len());
-    println!("With text:\n{contents}");
+    // println!("Length of file: {}", contents.len());
+    // println!("With text:\n{contents}");
+    println!("Content: {buffer}");
 }
